cf62bc94b1780d0dac0336ff96859f378e95dacb
Mayur244 <mayurghodaskar44@gmail.com>
2024-08-16 18:04:39 +0530

    Update project files

--- src/constant/youtube.js
+++ src/constant/youtube.js
@@ -1,5 +1,6 @@
 // named export 
-export const API_KEY = "AIzaSyANwDViZKkEEmSaBdfTaxq63hiGFJqB1Tg";
+export const API_KEY = process.env.REACT_APP_API_KEY;
+// export const API_KEY = "AIzaSyANwDViZKkEEmSaBdfTaxq63hiGFJqB1Tg";
 export const BASE_URL = "https://www.googleapis.com/youtube/v3"
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
 export const SEARCH_SUGGESTIONS_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
